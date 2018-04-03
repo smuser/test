1cf81e67469852ec3b58deb002640139cd6d6bf9
Shaheen Mirza <shaheen.mirza@infogain.com>
2018-04-03 16:33:18 +0530

    added search feature code and modified Login page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,9 +1,16 @@
 package com.qa.pages;
 
+import org.testng.annotations.Test;
+
 public class LoginPage {
 
 	public void login()
 	{
 		System.out.println("Login Page");
+	}
+	
+	public void loginSet()
+	{
+		System.out.println("Login Set");
 	}
 }
